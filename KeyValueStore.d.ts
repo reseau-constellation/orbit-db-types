b93758d2e66a2c1d36a1f5078aeb746ec4f6c8be
julienmalard <julien.malard@mail.mcgill.ca>
2023-07-13 12:09:49 +0200

    More specific KVStore types

--- KeyValueStore.d.ts
+++ KeyValueStore.d.ts
@@ -1,14 +1,14 @@
 declare module "orbit-db-kvstore" {
     import Store from "orbit-db-store";
 
-    export default class KeyValueStore<V> extends Store {
-        get(key: string): V;
+    export default class KeyValueStore<V extends {[clef: string]: unknown}> extends Store {
+        get<K extends keyof V>(key: K): V[K];
 
-        put(key: string, value: V, options?: {}): Promise<string>;
-        set(key: string, value: V, options?: {}): Promise<string>;
+        put<K extends keyof V>(key: K, value: V[K], options?: {}): Promise<string>;
+        set<K extends keyof V>(key: K, value: V[K], options?: {}): Promise<string>;
 
-        del(key: string, options?: {}): Promise<string>;
+        del<K extends keyof V>(key: K, options?: {}): Promise<string>;
 
-        all: {[key: string]: V};
+        all: V;
     }
 }
