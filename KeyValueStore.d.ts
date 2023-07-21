39da417c0a5a713b0a14bd9bdf2a504bc63991b3
julienmalard <julien.malard@mail.mcgill.ca>
2023-07-21 11:16:40 +0200

    Return Partial for KVStores

--- KeyValueStore.d.ts
+++ KeyValueStore.d.ts
@@ -2,13 +2,13 @@ declare module "orbit-db-kvstore" {
     import Store from "orbit-db-store";
 
     export default class KeyValueStore<V extends {[clef: string]: unknown}> extends Store {
-        get<K extends keyof V>(key: K): V[K];
+        get<K extends keyof V>(key: K): V[K] | undefined;
 
         put<K extends keyof V>(key: K, value: V[K], options?: {}): Promise<string>;
         set<K extends keyof V>(key: K, value: V[K], options?: {}): Promise<string>;
 
         del<K extends keyof V>(key: K, options?: {}): Promise<string>;
 
-        all: V;
+        all: Partial<V>;
     }
 }
