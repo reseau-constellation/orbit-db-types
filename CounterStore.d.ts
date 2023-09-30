99c93b84bdfdbdfbbd6f68ad7baf3b6f294f8fec
julienmalard <julien.malard@mail.mcgill.ca>
2023-09-30 12:06:09 +0200

    Update for orbitdb-1.0

--- CounterStore.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "orbit-db-counterstore" {
-    import Store from "orbit-db-store";
-
-    export default class CounterStore extends Store {
-        value: number;
-        
-        inc(value?: number): Promise<string>;
-    }
-}
--- OrbitDBAccessControllers/Utils.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "orbit-db-access-controllers/src/utils/ensure-ac-address.js" {
-    export default function ensureAddress(address: string): string
-}
